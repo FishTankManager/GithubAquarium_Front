eae5c320631d395a574e1f500f8a29f2e37bf88d
jongin <0422ll@snu.ac.kr>
2025-09-13 16:40:11 +0900

    [Style] add pre-commit

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,10 @@
 export default function MainPage() {
   return (
     <div className="min-h-screen bg-white">
-      <main className="flex flex-col justify-center items-center text-black">
+      <main className="flex flex-col items-center justify-center text-black">
         <p className="text-h1 font-sixtyfour">Main Page</p>
         <p className="text-h1 font-vt323">Main Page</p>
-        <p className="text-h1">메인페이지</p>
+        <p className="text-h1">메인 페이지</p>
       </main>
     </div>
   );
