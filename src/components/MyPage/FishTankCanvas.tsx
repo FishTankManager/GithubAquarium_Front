265ce081bc41da2fbf08d50b945528aae6358af8
eileen <eileen914@snu.ac.kr>
2025-09-17 14:02:34 +0900

    [Feat] FishTankCanvas & 어항 이미지(더미)

---
+++ src/components/MyPage/FishTankCanvas.tsx
@@ -0,0 +1,20 @@
+import { forwardRef } from "react";
+import { CanvasSize } from "@/types/aquarium";
+
+export default forwardRef<HTMLDivElement, { size: CanvasSize; bowlSrc?: string }>(
+  function FishTankCanvas({ size, bowlSrc = "/images/bowl.png" }, ref) {
+    return (
+      <div
+        ref={ref}
+        className="relative rounded border bg-white/70"
+        style={{ width: size.width, height: size.height }}
+      >
+        <img
+          src={bowlSrc}
+          alt="fish bowl"
+          className="pointer-events-none absolute inset-0 h-full w-full object-contain select-none"
+        />
+      </div>
+    );
+  },
+);
