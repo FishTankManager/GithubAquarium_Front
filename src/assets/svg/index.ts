64fea957608e098c651f037a5bf0388f79147e83
jongin <0422ll@snu.ac.kr>
2025-10-31 17:29:32 +0900

    feat: fix Header align, add Footer DEVELOPER/CONTACT link

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,5 +1,7 @@
 import GithubLogo from "./GithubLogo.svg?react";
 import GoogleLogo from "./GoogleLogo.svg?react";
 import FishTest from "./fish_test.svg?react";
+import LinkIcon from "./Link.svg?react";
+import MailIcon from "./Mail.svg?react";
 
-export { GithubLogo, GoogleLogo, FishTest };
+export { GithubLogo, GoogleLogo, FishTest, LinkIcon, MailIcon };
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,57 @@
+import { GithubLogo, MailIcon } from "@/assets/svg";
+
 export default function Footer() {
   return (
     <footer className="bg-[#c7af7c]">
-      <div className="text-h1 flex w-full flex-col items-center justify-center px-10 py-4 text-white">
-        <p className="w-full">
-          DEVELOPERS: Imggaggu, jay20012024, eileen914, Jongpippan, SuhyunBan
-        </p>
-        <p className="w-full">CONTACT: likelion@snu.ac.kr</p>
+      <div className="text-h1 flex w-full flex-col items-start justify-center px-10 py-4 text-white">
+        <div className="mb-1 flex items-center">
+          <GithubLogo className="mr-1.5 inline-block h-4 w-4" />
+          <span className="mr-2">DEVELOPERS: </span>
+          <span
+            className="flex cursor-pointer items-center hover:text-gray-800 hover:underline"
+            onClick={() => window.open("https://github.com/eileen914")}
+          >
+            eileen914
+          </span>
+          <span className="mr-2">,</span>
+          <span
+            className="flex cursor-pointer items-center hover:text-gray-800 hover:underline"
+            onClick={() => window.open("https://github.com/Imggaggu")}
+          >
+            Imggaggu
+          </span>
+          <span className="mr-2">,</span>
+          <span
+            className="flex cursor-pointer items-center hover:text-gray-800 hover:underline"
+            onClick={() => window.open("https://github.com/SuhyunBan")}
+          >
+            SuhyunBan
+          </span>
+          <span className="mr-2">,</span>
+          <span
+            className="flex cursor-pointer items-center hover:text-gray-800 hover:underline"
+            onClick={() => window.open("https://github.com/Jongpippan")}
+          >
+            Jongpippan
+          </span>
+          <span className="mr-2">,</span>
+          <span
+            className="flex cursor-pointer items-center hover:text-gray-800 hover:underline"
+            onClick={() => window.open("https://github.com/jay20012024")}
+          >
+            jay20012024
+          </span>
+        </div>
+        <div className="flex items-center">
+          <MailIcon className="mr-1.5 inline-block h-4 w-4" />
+          <span className="mr-2">CONTACT:</span>
+          <span
+            className="mr-2 flex cursor-pointer items-center hover:text-gray-800 hover:underline"
+            onClick={() => window.open("mailto:likelion@snu.ac.kr")}
+          >
+            likelion@snu.ac.kr
+          </span>
+        </div>
       </div>
     </footer>
   );
