51936b5a9d28ddce00f2408f87463d3bc4ab5137
suhyun <seraphina0911@snu.ac.kr>
2025-11-22 15:41:37 +0900

    [Fix] App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { MainPage, MyPage, ShopPage } from "./pages";
-import CollectionPage from "./pages/CollectionPage";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/my" element={<MyPage />} />
-        <Route path="/shop" element={<ShopPage />} />
-        <Route path="/collection" element={<CollectionPage />} />
-      </Routes>
-    </BrowserRouter>
 import { MainPage, MyPage, ShopPage, TestPage, GitHubCallbackPage } from "./pages";
+import CollectionPage from "./pages/CollectionPage";
 import { AuthProvider } from "@/auth/AuthContext";
 
 function App() {
@@ -25,6 +13,7 @@ function App() {
           <Route path="/shop" element={<ShopPage />} />
           <Route path="/test" element={<TestPage />} />
           <Route path="/auth/github/callback" element={<GitHubCallbackPage />} />
+          <Route path="/collection" element={<CollectionPage />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
